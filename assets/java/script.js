65af3b3aedf02ed0b9c6bb1ef94b20c78d03adf1
Sammy <sammy.cullinan@gmail.com>
2020-07-02 13:41:51 -0700

    error handling for search

--- assets/java/script.js
+++ assets/java/script.js
@@ -7,8 +7,14 @@ var searchBarEl = document.getElementById("search-bar");
 var searchCityHandler = function(event) {
     event.preventDefault();
     var cityName = citySearchEl.value.trim();
-    getCityInfo(cityName);
-}
+    if (cityName) {
+        getCityInfo(cityName);
+        citySearchEl.value = "";
+    } else {
+        alert("Please enter a city to see its results!")
+        return;
+    };
+};
 var getCityInfo = function(city, state) {
 
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + state + "&units=imperial&appid=7b788606d2ca3b8dec8a6e5ab63f1a3c")
@@ -18,7 +24,7 @@ var getCityInfo = function(city, state) {
             console.log(data, data.name, data.main.temp, data.main.humidity);
             });
         } else {
-            alert("Error: " + response.statusText);
+            alert("Error: City " + response.statusText + "! Remember to enter the city name correctly. For a more specific result enter the city and state separated by a comma.");
         }
     })
     .catch(function(error) {
